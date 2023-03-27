b95138eefcbd43c7d20ffadb7ca7157e19b8b94b
nickolss <nickolasmaiaraujo@gmail.com>
2023-03-27 15:10:48 -0300

    update teste.class.php

--- teste.class.php
+++ teste.class.php
@@ -8,23 +8,23 @@ class Teste{
         $pessoa = new Pessoa();
 
         $pessoa->setNome($_POST['nome']);
-        echo "Nome: $pessoa->getNome()";
+        echo "Nome:" . $pessoa->getNome();
         echo "<br>";
 
         $pessoa->setTelefone($_POST['telefone']);
-        echo "Telefone: $pessoa->getTelefone()";
+        echo "Telefone: " . $pessoa->getTelefone();
         echo "<br>";        
 
         $pessoa->setOrigem($_POST['origem']);
-        echo "Origem: $pessoa->getOrigem()";
+        echo "Origem: " . $pessoa->getOrigem();
         echo "<br>";
         
         $pessoa->setData($_POST['data']);
-        echo "Data: $pessoa->getData()";
+        echo "Data: " . $pessoa->getData();
         echo "<br>";
 
         $pessoa->setObservacao($_POST['observacao']);
-        echo "Observação: $pessoa->getObservacao()";
+        echo "Observação: " . $pessoa->getObservacao();
     }
 }new Teste();
 
