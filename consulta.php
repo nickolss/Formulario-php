8364a1d720b2b1bc1b3b1158bdf65a67d8dac9fd
nickolss <nickolasmaiaraujo@gmail.com>
2023-04-01 21:13:23 -0300

    Form created for data query

--- consulta.php
+++ consulta.php
@@ -6,8 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="assets/css/consulta.css">
 </head>
 
@@ -18,13 +21,16 @@
                 <nav class="navbar navbar-expand-lg">
                     <div class="container-fluid">
                         <a class="navbar-brand cabecalho__titulo" href="index.php">SISTEMA WEB</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav">
                                 <li class="nav-item">
-                                    <a class="nav-link cabecalho__menu" aria-current="page" href="index.php">Cadastrar</a>
+                                    <a class="nav-link cabecalho__menu" aria-current="page"
+                                        href="index.php">Cadastrar</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link cabecalho__menu ativo" href="consulta.php">Consultar</a>
@@ -43,68 +49,66 @@
         </div>
 
         <div class="tabelinha">
-            <table class="tabela">
-                <thead>
-                    <tr class="tabela__cabecalho">
-                        <td class="tabela__titulos">Nome</td>
-                        <td class="tabela__titulos">Telefone</td>
-                        <td class="tabela__titulos">Origem</td>
-                        <td class="tabela__titulos">Contato</td>
-                        <td class="tabela__titulos">Observação</td>
-                        <td class="tabela__titulos">Ação</td>
+            <table class="table">
+                <thead class="tabela__cabecalho">
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col">Origem</th>
+                        <th scope="col">Contato</th>
+                        <th scope="col">Observação</th>
+                        <th scope="col">Ação</th>
                     </tr>
                 </thead>
-
-                
-                    <tbody>
-                        <tr>
-                            <td class="tabela__conteudo">
-                                <?php
-                                    require_once("pessoa.class.php");
-                                    $pessoa = new Pessoa();
-                                    $pessoa->setNome($_POST['nome']);
-                                    echo $pessoa->getNome();
-                                ?>
-                            </td>
-
-                            <td class="tabela__conteudo">
-                                <?php
+                <tbody>
+                    <tr>
+                        <td class="tabela__conteudo">
+                            <?php
+                            require_once("pessoa.class.php");
+                            $pessoa = new Pessoa();
+                            $pessoa->setNome($_POST['nome']);
+                            echo $pessoa->getNome();
+                            ?>
+                        </td>
+                        <td class="tabela__conteudo">
+                            <?php
                                     require_once("pessoa.class.php");
                                     $pessoa = new Pessoa();
                                     $pessoa->setTelefone($_POST['telefone']);
                                     echo $pessoa->getTelefone();
                                 ?>
-                            </td>
-
-                            <td class="tabela__conteudo">
-                                <?php
+                        </td>
+                        <td class="tabela__conteudo">
+                            <?php
                                     require_once("pessoa.class.php");
                                     $pessoa = new Pessoa();
                                     $pessoa->setOrigem($_POST['origem']);
                                     echo $pessoa->getOrigem();
                                 ?>
-                            </td>
+                        </td>
 
-                            <td class="tabela__conteudo">
-                                <?php
+                        <td class="tabela__conteudo">
+                            <?php
                                     require_once("pessoa.class.php");
                                     $pessoa = new Pessoa();
                                     $pessoa->setData($_POST['data']);
                                     echo $pessoa->getData();
                                 ?>
-                            </td>
-                            
-                            <td class="tabela__conteudo">
-                                <?php
+                        </td>
+
+                        <td class="tabela__conteudo">
+                            <?php
                                     require_once("pessoa.class.php");
                                     $pessoa = new Pessoa();
                                     $pessoa->setObservacao($_POST['observacao']);
                                     echo $pessoa->getObservacao();
                                 ?>
-                            </td>
-                        </tr>
-                    </tbody>
-                
+                        </td>
+                        <td class="tabela__conteudo">
+                            <p>Editar</p>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
